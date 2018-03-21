7f67f2686f991257b07a72b6ef64e59bb624ce7a
tiangela <tas14@yandex.ru>
2018-03-21 21:54:16 +0300

    header-menu-btn

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+// Меню навигации
+var navMain = document.querySelector('.menu');
+var navToggle = document.querySelector('.menu__toggle');
+var scroll = document.querySelector('.scroll');
+
+navMain.classList.remove('menu--nojs');
+//scroll.classList.remove('scroll--js');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('menu--closed')) {
+    navMain.classList.remove('menu--closed');
+    navMain.classList.add('menu--open');
+  //  scroll.classList.add('scroll--js');
+
+  } else {
+    navMain.classList.add('menu--closed');
+    navMain.classList.remove('menu--open');
+//    scroll.classList.remove('scroll--js');
+  }
+});
