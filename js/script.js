b13e50198648f631d36abbe309b2b5eab1959351
tiangela <tas14@yandex.ru>
2018-03-24 17:04:44 +0300

    about, main, pets + test-js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,27 @@ navToggle.addEventListener('click', function() {
 //    scroll.classList.remove('scroll--js');
   }
 });
+
+//Карусель
+    var width = 270; // ширина изображения
+    var count = 1; // количество изображений
+
+    var carousel = document.getElementById('carousel');
+    var list = carousel.querySelector('.pets__list');
+    var listElems = carousel.querySelectorAll('.pets__item');
+
+    var position = 0; // текущий сдвиг влево
+
+    carousel.querySelector('.pets__arrow--prev').onclick = function() {
+      // сдвиг влево
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position + width * count, 0)
+     list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.pets__arrow--next').onclick = function() {
+      // сдвиг вправо
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position - width * count, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+    };
